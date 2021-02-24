8cd60eca819bda7d16901cff7d34ab4265d198bb
tfeyereisen <35351119+tfeyereisen@users.noreply.github.com>
2021-02-23 20:33:26 -0600

    github actions

--- build/setup_things.sh
+++ build/setup_things.sh
@@ -9,21 +9,19 @@ sudo apt-get -y install net-tools zip jq
 
 # Install Consul and Nomad
 export CONSUL_VERSION="1.9.3"
-https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_amd64.zip
-unzip consul_${CONSUL_VERSION}_linux_amd64.zip
+sudo wget https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_amd64.zip
+sudo unzip consul_${CONSUL_VERSION}_linux_amd64.zip
 sudo chown root:root consul
 sudo mv consul /usr/local/bin/
 sudo mkdir -p /etc/consul.d
 sudo mkdir -p /opt/consul
-sudo chmod -R 777 /opt/nomad
+sudo chmod -R 777 /opt/consul
 
 export NOMAD_VERSION="1.0.1"
-curl --silent --remote-name https://releases.hashicorp.com/nomad/${NOMAD_VERSION}/nomad_${NOMAD_VERSION}_linux_amd64.zip
-unzip nomad_${NOMAD_VERSION}_linux_amd64.zip
+sudo wget https://releases.hashicorp.com/nomad/${NOMAD_VERSION}/nomad_${NOMAD_VERSION}_linux_amd64.zip
+sudo unzip nomad_${NOMAD_VERSION}_linux_amd64.zip
 sudo chown root:root nomad
 sudo mv nomad /usr/local/bin/
-sudo mkdir -p /etc/nomad-server.d
-sudo mkdir -p /etc/nomad-client.d
-sudo mkdir -p /opt/nomad/server
-sudo mkdir -p /opt/nomad/clent
+sudo mkdir -p /etc/{nomad-server.d,nomad-client.d}
+sudo mkdir -p /opt/nomad/{server,client}
 sudo chmod -R 777 /opt/nomad
