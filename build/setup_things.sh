ab8673387629e6787f7b812657acc02e8f9117d8
tfeyereisen <35351119+tfeyereisen@users.noreply.github.com>
2021-02-23 20:54:52 -0600

    fix consul unit file

--- build/setup_things.sh
+++ build/setup_things.sh
@@ -7,7 +7,7 @@ sudo snap install amazon-ssm-agent --classic
 sudo apt-get update
 sudo apt-get -y install net-tools zip jq
 
-# Install Consul and Nomad
+# Install Consul
 export CONSUL_VERSION="1.9.3"
 sudo wget https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_amd64.zip
 sudo unzip consul_${CONSUL_VERSION}_linux_amd64.zip
@@ -17,6 +17,7 @@ sudo mkdir -p /etc/consul.d
 sudo mkdir -p /opt/consul
 sudo chmod -R 777 /opt/consul
 
+# Install Nomad
 export NOMAD_VERSION="1.0.1"
 sudo wget https://releases.hashicorp.com/nomad/${NOMAD_VERSION}/nomad_${NOMAD_VERSION}_linux_amd64.zip
 sudo unzip nomad_${NOMAD_VERSION}_linux_amd64.zip
