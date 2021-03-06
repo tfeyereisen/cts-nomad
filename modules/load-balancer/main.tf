79fc4d3a1eae739323cd637c980d4cfd429f3188
tfeyereisen <35351119+tfeyereisen@users.noreply.github.com>
2021-03-06 11:46:53 -0600

    Update main.tf

--- modules/load-balancer/main.tf
+++ modules/load-balancer/main.tf
@@ -1,6 +1,6 @@
 
 variable "consul_terraform_sync_image" {
-  default = "hashicorp/consul-terraform-sync:0.1.0-beta"
+  default = "hashicorp/consul-terraform-sync:0.1.0-techpreview2"
 }
 
 locals {
@@ -21,8 +21,8 @@ locals {
 resource "nomad_job" "sync" {
   depends_on = [aws_lb.consul-ingress-alb]
   jobspec = templatefile("${path.module}/sync-job.hcl", {
-    region                   = var.region,
-    sync_container_image     = var.consul_terraform_sync_image,
+    region                   = var.region
+    sync_container_image     = var.consul_terraform_sync_image
     sync_config              = local.sync_config
     sync_module_main         = local.sync_module_main
     sync_module_variables    = local.sync_module_variables
